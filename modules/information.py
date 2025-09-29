f5809e6348a758b5557b66449b1c140c2d5e84b8
EduardoGladzik <eduardogladzik@gmail.com>
2025-09-29 12:46:37 -0300

    Classe 'SystemInformation()' criada. dicionário 'system_informations' criado para armazenar as informações necessárias. Função 'gather_and_export_information()' renomeada para 'write_information()', agora ela cumpre apenas a responsabilidade de escrever de forma dinâmica as informações no arquivo de texto.

--- modules/information.py
+++ modules/information.py
@@ -1,59 +1,28 @@
 import subprocess
 import getpass
-import datetime
 import os
 
-def gather_and_export_information():
-    # Define the filename with date and time to avoid overwriting
-    date_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"system_info_{date_time}.txt"
-
-    # Get the current user's desktop path
+class SystemInformation():
+    filename = "system_info.txt"
     desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-    full_path = os.path.join(desktop_path, filename)
+    file_path = os.path.join(desktop_path, filename)
 
-    print(f"--- Exportando informações para o arquivo '{full_path}' ---")
+    system_informations = {
+        "computer_name": subprocess.run(["wmic", "computersystem", "get", "name"], capture_output=True, text=True, check=True).stdout.strip().split()[1],
+        "computer_model": subprocess.run(["wmic", "computersystem", "get", "model"], capture_output=True, text=True, check=True).stdout.strip().split()[1],
+        "serial_number": subprocess.run(["wmic", "bios", "get", "serialnumber"], capture_output=True, text=True, check=True).stdout.strip().split()[1],
+        "logged_user": getpass.getuser(),
+        "ip_information": subprocess.run(["ipconfig"], capture_output=True, text=True, check=True).stdout
+    }
 
-    # Open the file in write mode ('w') on the desktop path
-    with open(full_path, "w", encoding="utf-8") as file:
-        # Title
-        file.write("### Relatório de Informações do Sistema ###\n\n")
-        file.write(f"Data e Hora: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n\n")
 
-        # Computer name
-        file.write("1. Nome do computador: ")
+    def write_information(file, info, data):
+        """
+        Export system information to "system_info.txt".
+        """
         try:
-            computer_name = subprocess.run(["wmic", "computersystem", "get", "name"], capture_output=True, text=True, check=True)
-            file.write(f"{computer_name.stdout.strip().split()[1]}\n\n")
-        except subprocess.CalledProcessError as e:
-            file.write(f"Erro ao executar comando: {e}\n\n")
-
-        # Computer model
-        file.write("2. Modelo do computador: ")
-        try:
-            computer_model = subprocess.run(["wmic", "computersystem", "get", "model"], capture_output=True, text=True, check=True)
-            file.write(f"{computer_model.stdout.strip().split()[1]}\n\n")
-        except subprocess.CalledProcessError as e:
-            file.write(f"Erro ao executar comando: {e}\n\n")
-
-        # Serial number
-        file.write("3. Número de série: ")
-        try:
-            serial_number = subprocess.run(["wmic", "bios", "get", "serialnumber"], capture_output=True, text=True, check=True)
-            file.write(f"{serial_number.stdout.strip().split()[1]}\n\n")
-        except subprocess.CalledProcessError as e:
-            file.write(f"   Erro ao executar comando: {e}\n\n")
-
-        # Logged user
-        file.write("4. Usuário logado: ")
-        file.write(f"   {getpass.getuser()}\n\n")
-
-        # IP information
-        file.write("5. Informações de IP:\n")
-        try:
-            result = subprocess.run(["ipconfig"], capture_output=True, text=True, check=True)
-            file.write(result.stdout + "\n")
-        except subprocess.CalledProcessError as e:
-            file.write(f"   Erro ao executar comando: {e}\n\n")
-
-    print(f"Exportação concluída! Verifique o arquivo '{full_path}'.")
+            file.write(f"--- {info.replace('_', ' ').title()} ---\n")
+            file.write(f"{data}\n\n")
+        except Exception as e:
+            print(f"Erro ao escrever informações: {e}")
+            return
